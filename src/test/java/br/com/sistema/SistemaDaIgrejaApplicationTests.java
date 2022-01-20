740be938661b0b4451450d8904152eefd05ff2f0
Danielscript1 <danielsousagit@gmail.com>
2022-01-20 14:03:26 -0300

    projetoInicial

---
+++ src/test/java/br/com/sistema/SistemaDaIgrejaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sistema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDaIgrejaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
